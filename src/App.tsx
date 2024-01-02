0e2fbf6586c5a15d2a4b95e8029106726911975c
Mr.Chetarin <chetarin.won@gmail.com>
2024-01-02 14:54:20 +0700

    inti

---
+++ src/App.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import { Button, Textarea } from "@material-tailwind/react";
+
+function App() {
+  const [inputString, setInputString] = useState("");
+  const [result, setResult] = useState("");
+
+  function removeSignAndCommas(text: string): string {
+    // ใช้ Regular Expression เพื่อแยกตัวเลขและลบเครื่องหมาย
+    const result = text.replace(/[^\d.]/g, "");
+    return result;
+  }
+
+  function processMultipleLines(input: string): string {
+    const lines = input.split("\n"); // แยกข้อความเป็นแถวหลายๆ แถว
+
+    const outputLines = lines.map((line) => {
+      const result = removeSignAndCommas(line);
+      return result;
+    });
+
+    return outputLines.join("\n"); // รวมข้อความเป็นแถวหลายๆ แถว
+  }
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    setResult(processMultipleLines(inputString));
+    console.log(result);
+  };
+
+  return (
+    <>
+      <div className='container mx-auto my-10 px-4'>
+        <h1 className='text-2xl font-bold text-center'>Remove Sign String</h1>
+
+        <Textarea
+          label='Input String'
+          value={inputString}
+          onChange={(e) => setInputString(e.target.value)}
+          rows={10}
+          resize={true}
+        ></Textarea>
+
+        <div className='flex justify-center my-4'>
+          <Button
+            type='button'
+            onClick={handleSubmit}
+            variant='gradient'
+            placeholder='แปลงผลลัพธ์'
+          >
+            แปลงผลลัพธ์
+          </Button>
+        </div>
+
+        <div className='py-2'>
+          <Textarea
+            id='resultTxt'
+            label='Result'
+            value={result}
+            rows={10}
+            resize={true}
+          ></Textarea>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import * as path from "path";
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig(() => {
+  const env = Object.entries(loadEnv("mock", process.cwd(), "")).reduce(
+    (prev, [key, val]) => ({
+      ...prev,
+      [key]: val,
+    }),
+    {}
+  ) as any;
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src/"),
+        "@assets": `${path.resolve(__dirname, "./src/assets/")}`,
+      },
+    },
+    base: "/",
+    define: {
+      APP_VERSION: JSON.stringify(env.npm_package_version),
+    },
+  };
+});
